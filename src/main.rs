997b3435abb1e7bb6b8d7a7162382a49cdf53383
Zoltán Balázs <balazs.zoltan2016@gmail.com>
2021-09-01 21:41:08 +0200

    Refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod calendar;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if course::check_if_csv_exists("WEBPROGEG") {
-        course::read_csv_to_text("WEBPROGEG");
+        course::read_csv_to_text("WEBPROGEG")?;
     } else {
         course::get_course_info("2021-2022-1", "Tárgykód", "WEBPROGEG").await?;
     }
 
-    calendar::write_courses_to_ics("");
-    
+    calendar::write_courses_to_ics("")?;
+
     Ok(())
 }
